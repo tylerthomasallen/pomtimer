8d4807a6ee9b8946e4905b77008218aa77a82f4c
Tyler <tylerthomasallen@gmail.com>
2019-02-04 12:33:45 -0700

    Add instructions

--- src/components/instructions/index.js
+++ src/components/instructions/index.js
@@ -4,6 +4,13 @@ const Instructions = (props) => {
     return(
         <div className="instructions-container">
             <h1>HOW IT WORKS</h1>
+            <div className="instruction-items">
+            <span>Step 1: Add tasks todo</span>
+            <span>Step 2: Work for 25 minutes or one "Pomodoro"</span>
+            <span>Step 3: Mark your completed tasks!</span>
+            <span>Step 4: Take a short break</span>
+            <span>Step 5: Repeat steps 1-4. After 4 Pomodoro, take a long break :)</span>
+            </div>
         </div>
     )
 }
