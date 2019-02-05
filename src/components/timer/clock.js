de0bd5bc8da7b3bda4b1077e6ddcde99a00d10cc
Tyler <tylerthomasallen@gmail.com>
2019-02-05 09:03:09 -0700

    Add google analytics tag

--- src/components/timer/clock.js
+++ src/components/timer/clock.js
@@ -4,13 +4,13 @@ import { POM_TIMER, SHORT_BREAK, LONG_BREAK } from '../../constants';
 const Clock = ({displayedTime, play, pause, timeType}) => {
     let gif;
     if (timeType === POM_TIMER.time) {
-        gif = <iframe src="https://giphy.com/embed/IPbS5R4fSUl5S" width="400" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+        gif = <iframe src="https://giphy.com/embed/IPbS5R4fSUl5S" width="400" height="480" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
     } else if (timeType === SHORT_BREAK.time) {
-        gif = <iframe src="https://giphy.com/embed/muCo9BLS7vjErTON27" width="400" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+        gif = <iframe src="https://giphy.com/embed/muCo9BLS7vjErTON27" width="400" height="480" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
     } else if (timeType === LONG_BREAK.time) {
-        gif = <iframe src="https://giphy.com/embed/3ov9jVajUykLpFTMQM" width="400" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+        gif = <iframe src="https://giphy.com/embed/3ov9jVajUykLpFTMQM" width="400" height="480" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
     } else {
-        gif = <iframe src="https://giphy.com/embed/w7mLEAMcpjrpe" width="400" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>  
+        gif = <iframe src="https://giphy.com/embed/w7mLEAMcpjrpe" width="400" height="480" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>  
         
     }
     return(
